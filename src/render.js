0f38930f8e6c5706d372d69b64029a9cbb424cb1
Denny <dennythecoder@gmail.com>
2017-10-11 14:00:03 -0500

    check for el

--- src/render.js
+++ src/render.js
@@ -2,6 +2,7 @@
 const regex = /\{\{((?:.|\n)+?)\}\}/g;
 
 export default function(){
+    if(this.$el.innerHTML) return;
     const myHTML = this.$el.innerHTML;
     let model = this.model;
     let html = myHTML.replace(regex,function(match){
