6c00f3b96820fbf2b025429244de9296d042187d
Denny <dennythecoder@gmail.com>
2017-10-11 14:59:19 -0500

    refactor: spacing

--- src/mount.js
+++ src/mount.js
@@ -1,12 +1,9 @@
 export default function(id){
-
     const el = document.getElementById(id);
     if(el){
       this.$el = el;
       this.render();
-      
     }else{
       console.warn('Element selector not valid');
     }
-
 }
