06bb955fc4a796a294fcdeea1df7350377719774
Denny <dennythecoder@gmail.com>
2017-10-11 10:48:43 -0500

    Added comment for build

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,5 @@
-
+::call this file from the base folder ./build/build.bat
+@echo off
 call rollup ./src/index.js --o ./dist/naf.js --f iife --name Naf
 call rollup ./src/index.js --o ./dist/naf.common.js --f cjs
 call node ./build/removeStrict.js
